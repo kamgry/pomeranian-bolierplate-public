f59328f64f4a58c5c2c3131df43341b952b6c5c5
Kamil Grygiel <kamil@hausegger-inc.com>
2023-04-27 19:33:27 +0200

    Create input with button

--- src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
+++ src/App/Exercises/ReactExercises/WelcomeView/WelcomeView.jsx
@@ -1,5 +1,10 @@
 import './styles.css';
 
 export const WelcomeView = () => {
-  return <div>welcome</div>;
+  return (
+    <div className="welcome-view">
+      <input placeholder="text" onChange={(e) => console.log(e)} />
+      <button onClick={(e) => console.log(e)}>test</button>
+    </div>
+  );
 };
