f2a23cddf0a6834c887409bdbd22ab7ae6a071bb
Kamil Grygiel <kamil@hausegger-inc.com>
2023-04-27 17:43:45 +0200

    Add welcome view to routes

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,10 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
+import { welcomeViewRouterMetaData } from './WelcomeView/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData];
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  welcomeViewRouterMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
