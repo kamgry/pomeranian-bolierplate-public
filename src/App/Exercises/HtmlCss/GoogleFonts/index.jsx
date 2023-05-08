276b35a08cc038b98204e011c08941e596c37b42
Kamil Grygiel <kamil@hausegger-inc.com>
2023-05-08 20:16:08 +0200

    Add custom fonts

---
+++ src/App/Exercises/HtmlCss/GoogleFonts/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export function GoogleFonts() {
+  return (
+    <div className="google-fonts">
+      <div className="google-fonts">google fonts aąęćśłó</div>
+      <div className="google-fonts">google fonts</div>
+      <div className="google-fonts">google fonts</div>
+      <div className="google-fonts">google fonts</div>
+    </div>
+  );
+}
